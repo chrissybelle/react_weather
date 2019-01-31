bf4501c3df9d867d77bad3c0cfb6ceda0879763c
cbelle <christabelle.chien@gmail.com>
2019-01-31 10:11:26 -0500

    updates-date formatting

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,13 +2,28 @@ import React from "react";
 import "./card.css";
 
 class Card extends React.Component {
+
     render() {
+
         console.log(`card.js: ${this.props.weather.temp}`);
+        const extractedDate = this.props.weather.id.slice(0, 11); //removes timestamp from API data
+        console.log(`extracted: ${extractedDate}`);
+        const formattedDate = extractedDate.slice(5, 10) + "-" + extractedDate.slice(0, 5).slice(0, -1); //reformats date from YYYY-MM-DD to MM-DD-YYYY
+        console.log(`formatted: ${formattedDate}`);
+        const weatherDate = new Date(formattedDate);
+        console.log(`WEATHER DAY ${weatherDate}`);
+        const daysOfTheWeek = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+        let weatherDay = daysOfTheWeek[weatherDate.getDay()];
+        console.log(`final day: ${weatherDay}`);
+       
+
+
 
         return (
             <div className="card-day">
-                <p>{this.props.weather.id.slice(0, 11)}</p>
-                <p>{this.props.weather.temp}</p>
+                <p>{formattedDate}</p>
+                <p>{weatherDay}</p>
+                <p>{this.props.weather.temp}°F</p>
                 <img src={process.env.PUBLIC_URL + "/icons/" + this.props.weather.icon + ".png"} alt={this.props.weather.weather} />
                 <p>{this.props.weather.weather}</p>
                 <p>{this.props.weather.details}</p>
