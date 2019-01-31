f2e746a4fdce0806d8133c1de3c6dd33cdbc61d9
cbelle <christabelle.chien@gmail.com>
2019-01-31 15:21:35 -0500

    updates to landing screen

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -1,16 +1,27 @@
 import React from "react";
 import Card from "./Card";
 import "./cardContainer.css";
+import landingImg from "../landing.png";
 
 class CardContainer extends React.Component {
     render() {
         console.log(`cardcontainer.js: ${this.props.results}`);
         return (
-            <div className="card-container">
-                {this.props.results.map(result => {
-                    return <Card weather={result} key={result.id}/>
-                })}
-            </div>
+            this.props.results[1] ?
+
+                <div className="card-container">
+                    {this.props.results.map(result => {
+                        return <Card weather={result} key={result.id} />
+                    })}
+                </div>
+
+                :
+
+                <div className="landing">
+                    <img className="landing-img" src={landingImg} alt="weather_search"/>
+                    <h1 className="landing-header">Weather Search</h1>
+                </div>
+
         );
     }
 };
